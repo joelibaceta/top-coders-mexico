276c9b39b9780a070668c872caeaa068f4a6ac57
Joel Ibaceta <joelibaceta@MacBook-Pro-de-Joel.local>
2023-03-06 23:53:16 -0500

    less users

--- _plugins/top_users_tag.rb
+++ _plugins/top_users_tag.rb
@@ -109,7 +109,7 @@ module Jekyll
             top_users = []
 
             location = ENV['LOCATION']
-            uri = "https://api.github.com/search/users?q=#{location}+followers:>10+repos:>10+type:user&per_page=50&page=1&sort=followers&order=desc"
+            uri = "https://api.github.com/search/users?q=#{location}+followers:>10+repos:>10+type:user&per_page=30&page=1&sort=followers&order=desc"
             p "getting data from to #{uri}"
             
             raw_response = make_get_request(uri)
